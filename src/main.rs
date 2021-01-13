741f53937f4c355f6dc161e69279dd25bdfa550d
xinster <xinster@users.noreply.github.com>
2021-01-13 19:48:17 +0800

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+
+    let params = vec![123,23456,6788];
+    match sum(&params){
+        Some(v)=>println!("total value is {}", v),
+        None =>println!("overflow")
+    }
+}
+
+fn sum(numbers:&[u32])->Option<u32>{
+    let mut total:u32 = 0;
+    for val in numbers.iter(){
+        match total.checked_add(*val){
+            Some(v)=>{total = v},
+            None=>{return None}
+        }
+    }
+    Some(total)
 }
